f7e1445f965d3cec23a289d13604d9cf0da0b073
Andre Gama <andre_gama789@hotmail.com>
2020-08-15 14:53:59 -0300

    nova coluna na tabela users

---
+++ database/migrations/2020_08_15_175033_add_column_type_user_id_type_user_to_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnTypeUserIdTypeUserToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('type_users_id_type_user')->after('addresses_id_address');
+            $table->foreign('type_users_id_type_user')
+                  ->references('id_type_user')
+                  ->on('type_users')
+                  ->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('type_users_id_type_user');
+        });
+    }
+}
