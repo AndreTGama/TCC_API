294dfde4143630e0293e7f6f22b394be35b2cdb0
Your Name <you@example.com>
2020-09-13 17:08:56 -0300

    criei nova tabela[verify_code]

---
+++ app/Model/verify_code.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class verify_code extends Model
+{
+    protected $table='verify_codes';
+
+    protected $fillable = [
+		'code'
+    ];
+}
